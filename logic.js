ac62e368470595fdd76d40c0cbb7f2765dc46666
tzlilhaim <tzlilhaim480@gmail.com>
2020-08-20 17:19:50 +0300

    fixes and adjustments for working with the controller

--- logic.js
+++ logic.js
@@ -1,29 +1,11 @@
 // Modules
 
 const Tweeter = function () {
-    const _posts = [{
-        text: "First post!",
-        id: "p1",
-        comments: [
-            { id: "c1", text: "First comment on first post!" },
-            { id: "c2", text: "Second comment on first post!!" },
-            { id: "c3", text: "Third comment on first post!!!" }
-        ]
-    },
-    {
-        text: "Aw man, I wanted to be first",
-        id: "p2",
-        comments: [
-            { id: "c4", text: "Don't wory second poster, you'll be first one day." },
-            { id: "c5", text: "Yeah, believe in yourself!" },
-            { id: "c6", text: "Haha second place what a joke." }
-        ]
-    }]
+    const _posts = []
     let postIdCounter = _posts.length
-    let commentIdCounter = 6
+    let commentIdCounter = 0
     const getPosts = () => _posts
     const addPost = (pText) => {
-        debugger
         _posts.push({ text: pText, id: "p" + (postIdCounter + 1), comments: [] })
         postIdCounter++
     }
@@ -36,7 +18,6 @@ const Tweeter = function () {
     }
     const addComment = (cText, postID) => {
         for (p of _posts) {
-            debugger
             if (p.id == postID) {
                 p.comments.push({id: "c" + (commentIdCounter + 1), text: cText,})
             }
@@ -44,7 +25,6 @@ const Tweeter = function () {
         commentIdCounter++
     }
     const removeComment = (postID, commentID) => {
-        debugger
         for (p of _posts) {
             if (p.id == postID) {
                 for (c of p.comments) {
