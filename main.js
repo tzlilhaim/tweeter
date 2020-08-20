f0f08849755d5af0033dfa4d5efe2d70082346fe
tzlilhaim <tzlilhaim480@gmail.com>
2020-08-20 17:18:47 +0300

    added controller step

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
 const tweeter = Tweeter()
 const renderer = Renderer()
 
-console.log(tweeter.getPosts())
-debugger
-renderer.renderPosts(tweeter.getPosts())
-debugger
-renderer.renderPosts(tweeter.getPosts())
+const render = () => renderer.renderPosts(tweeter.getPosts())
+render()
+
+// On click of Twit button
+const post = () => {
+    tweeter.addPost($("#input").val())
+    render()
+}
+
+// On click of delete post button
+$("#posts").on("click", ".delete", function () {
+    tweeter.removePost($(this).closest(".post").data().id)
+    render()
+})
+
+// On click of add comment button
+$("#posts").on("click", "button:contains(Comment)", function () {
+    tweeter.addComment(
+        $(this).closest(".comments").find("input").val(),
+        $(this).closest(".post").data().id)
+    render()
+})
+
+// On click of delete comment button
+$("#posts").on("click", ".delete-comment", function () {
+    tweeter.removeComment(
+        $(this).closest(".post").data().id,
+        $(this).closest("div").data().id
+    )
+    render()
+})
